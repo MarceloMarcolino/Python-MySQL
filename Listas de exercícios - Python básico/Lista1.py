7034b3e2a3d9f3d20841cfc7bf0ed0de5a35405c
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-07-04 11:21:14 -0300

    Update Lista1.py

--- Listas de exercícios - Python básico/Lista1.py
+++ Listas de exercícios - Python básico/Lista1.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from math import sqrt
 
 # Exercício 1
 idade = int(input("Digite sua idade: "))
@@ -24,6 +25,21 @@ else:
 		print("O usuário está reprovado")
 
 # Exercício 3
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+c = int(input("Digite o valor de C: "))
+delta = pow(b, 2) - (4*a*c)
+try:
+	raiz_delta = sqrt(delta)
+except ValueError:
+	raise ValueError("Delta negativo, então não admite solução real.") from None
+if raiz_delta >= 0:
+	x1 = (-b + raiz_delta)/(2*a)
+	x2 = (-b - raiz_delta)/(2*a)
+	if x1 == x2:
+		print("A raiz é", x1)
+	else:
+		print("As raízes são", x1, "e", x2)
 
 # Exercício 4
 
