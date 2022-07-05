3bfb3e96892d807d39466a9ba843616ae6b2425c
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-07-04 22:22:31 -0300

    Create exercise.py

---
+++ Listas de exercícios - Python básico/exercise.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# USUARIO
+
+idade = 23
+
+# Verifique se usuario é maior de idade ou menor de idade
+if idade >= 18:
+	print("Maior de idade")
+else:
+	print("Menor de idade")
