aae0c15281f3f422db1b29673800aeea6eac5d08
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-08-13 00:26:11 -0300

    Finished advanced python

--- Python avançado/map.py
+++ Python avançado/map.py
@@ -10,8 +10,10 @@ valor = [1, 2, 3, 4, 5]
 print(dobro(valor))
 
 valor_dobrado = map(dobro, valor)
+"""
+# Por algum motivo que desconheço, o map fica vazio ao realizar o código comentado.
 for v in valor_dobrado:
 	print(v)
-
+"""
 valor_dobrado = list(valor_dobrado)
 print(valor_dobrado)
--- Python avançado/zip.py
+++ Python avançado/zip.py
@@ -7,6 +7,5 @@ lista3 = ["R$ 2,00", "R$ 5,00", "Não tem preço", "Não tem preço", "Não tem 
 for numero, nome in zip(lista1, lista2):
 	print(numero, nome)
 
-
 for numero, nome, valor in zip(lista1, lista2, lista3):
 	print(numero, nome, valor)
